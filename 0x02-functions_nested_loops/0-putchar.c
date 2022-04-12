ff69d1d1ff3d3503c6f9483868551a9894e7f69e
yandys15053 <101865406+yandys15053@users.noreply.github.com>
2022-04-12 21:47:10 +0000

    Update 0-putchar.c
    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Print _putchar followed by new line
- * Return: 0
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
-	{
-	char c[9] = "_putchar";
-	int i = 0;
-	while (i <  8)
-	{
-		_putchar(c[i]);
-		i++;
-	
-	}
+{
+    char text[9] = "_putchar";
+    int i = 0;
 
-	_putchar('\n');
-	return (0);
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(text[i]);
+    }
+    _putchar('\n');
 
-	}
+    return (0);
+}
