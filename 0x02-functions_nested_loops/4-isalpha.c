ff6957d2f92369e3f9dbead28166e1c0ed0d4486
yandys15053 <yandymans15053@gmail.com>
2022-04-12 16:10:39 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+int _isalpha(int c);
+	{
+		if (c >= 'a' && c <= 'z')
+		{
+			return (1);
+		}
+		
+		else if (c >= 'A' && c <= 'Z')
+			{ 
+				return (0);
+			}
+		else {
+			return (0);
+		}
+	}
